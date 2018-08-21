f4f143bc04661a76a8272275ef8d4d9f1935d0d4
ShawnShoper <xiehao,930825>
2018-08-21 18:15:20 +0800

    modify type field for test

--- hsweb-task-core/src/main/java/org/hswebframework/web/task/JobDetail.java
+++ hsweb-task-core/src/main/java/org/hswebframework/web/task/JobDetail.java
@@ -43,10 +43,10 @@ public class JobDetail {
     /**
      * 任务类型,该字段用于content组合用于不同类型任务的支持,如: java-method,script,jar 等
      */
-    private String type;
+    private String taskType;
 
     /**
-     * 任务内容,{@link this#type}字段不同,配置格式也不同,具体格式由{@link TaskRunnableFactory}进行处理
+     * 任务内容,{@link this#taskType}字段不同,配置格式也不同,具体格式由{@link TaskRunnableFactory}进行处理
      *
      * @see TaskRunnableFactory
      */
